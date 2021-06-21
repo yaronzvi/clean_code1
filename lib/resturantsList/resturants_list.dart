6a9776eb2ee409f831aa11b867d1a7fab215e2be
yaronzvi <yaron.zvi@gmail.com>
2021-06-21 15:27:56 +0300

    try1 compile to web

--- lib/resturantsList/resturants_list.dart
+++ lib/resturantsList/resturants_list.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:first_proj/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 // final CollectionReference restCollection =
 //     FirebaseFirestore.instance.collection(kRestaurantCollection);
@@ -16,10 +17,17 @@ class ResturantsList extends StatefulWidget {
 class _ResturantsListState extends State<ResturantsList> {
   @override
   Widget build(BuildContext context) {
+    // final _firestore = FirebaseFirestore.instance
+    //     .collection(kRestaurantCollection)
+    //     .where('name', isGreaterThanOrEqualTo: widget.manaName)
+    //     .where('name', isLessThan: widget.manaName + 'z');
+
     final _firestore = FirebaseFirestore.instance
         .collection(kRestaurantCollection)
         .where('name', isGreaterThanOrEqualTo: widget.manaName)
-        .where('name', isLessThan: widget.manaName + 'z');
+        .where('name', isLessThan: widget.manaName + 'z')
+        .orderBy('name')
+        .limit(3);
 
     return Scaffold(
       appBar: AppBar(
@@ -37,8 +45,17 @@ class _ResturantsListState extends State<ResturantsList> {
             return ListView(
               children: snapshot.data!.docs.map((doc) {
                 return Card(
-                  child: ListTile(
-                    title: Text(doc.data().toString()),
+                  child: Column(
+                    children: [
+                      Padding(padding: const EdgeInsets.all(8.0)),
+                      ListTile(
+                        title: Text(
+                          doc.data().toString(),
+                          style: TextStyle(fontSize: 20.0),
+                        ),
+                      ),
+                      Padding(padding: const EdgeInsets.all(8.0)),
+                    ],
                   ),
                 );
               }).toList(),
